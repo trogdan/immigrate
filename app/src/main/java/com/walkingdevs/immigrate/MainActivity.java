372e0101b3e0c401653da56bccbfb9cd7cc55544
Aleli Evangelista <hello@alelievangelista.com>
2015-11-10 10:23:13 -0800

    First page

--- app/src/main/java/com/walkingdevs/immigrate/MainActivity.java
+++ app/src/main/java/com/walkingdevs/immigrate/MainActivity.java
@@ -1,11 +1,8 @@
 package com.walkingdevs.immigrate;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +14,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /**FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own special *trogdan* action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
