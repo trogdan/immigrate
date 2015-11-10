ecc6cdb2ddceb9f82c7c581da07869059b096e5f
trogdan <mr.trogdan@gmail.com>
2015-11-10 15:53:10 -0800

    Reverting weirdness

--- app/src/main/java/com/walkingdevs/immigrate/MainActivity.java
+++ app/src/main/java/com/walkingdevs/immigrate/MainActivity.java
@@ -1,16 +1,10 @@
 package com.walkingdevs.immigrate;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,7 +14,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        /**FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
     }
 
     @Override
