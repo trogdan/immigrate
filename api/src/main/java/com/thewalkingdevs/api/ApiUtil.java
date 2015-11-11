831d542c702378acccf01843f1c2f9dc9519633c
Levan Iordanishvili <levandev9053@gmail.com>
2015-11-10 19:57:35 -0500

    Create working places/essentials endpoint

--- api/src/main/java/com/thewalkingdevs/api/ApiUtil.java
+++ api/src/main/java/com/thewalkingdevs/api/ApiUtil.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thewalkingdevs.api.constants.AppConstants;
+import com.thewalkingdevs.api.constants.AppConstantsPrivate;
 import com.thewalkingdevs.api.data.ImmigrateDAO;
 import com.thewalkingdevs.api.model.CityIndices;
 import com.thewalkingdevs.api.model.CityPrices;
+import com.thewalkingdevs.api.model.Places;
+import com.thewalkingdevs.api.model.Place;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,12 +18,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * API Util for api calls.
  */
 public final class ApiUtil {
     private static ImmigrateDAO DAO = new ImmigrateDAO();
+
+    private static final Logger LOG = Logger.getLogger(ApiUtil.class.getName());
 
     private ApiUtil() {}
 
@@ -29,6 +37,8 @@ public final class ApiUtil {
         // so that they can be closed in the finally block.
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
+
+        LOG.info(urlStr);
 
         try {
             URL url = new URL(urlStr);
@@ -110,8 +120,110 @@ public final class ApiUtil {
         return requestUrl.toString();
     }
 
-    private String getQueryParam(String key, String value){
+    private static String getQueryParam(String key, String value){
         return "&" + key + "=" + value;
+    }
+
+    private static List<Place> getPostOffices(String location){
+
+        StringBuilder sb = getPlacesQueryBase(location);
+        sb.append(getQueryParam(AppConstants.PLACES_QUERY_NAME, "Post%20Office"));
+
+        String postOfficesJson = callAPI(DAO.getRequestUrl(AppConstants.PLACES_QUERY_BASE, sb.toString()).toString());
+        Places immigrationOfficeContainer = convert(postOfficesJson, Places.class);
+        List<Place> postOfficesResults = immigrationOfficeContainer.getResults();
+        return postOfficesResults;
+    }
+
+
+    private static List<Place> getImmigrationOffices(String location){
+
+        StringBuilder sb = getPlacesQueryBase(location);
+        sb.append(getQueryParam(AppConstants.PLACES_QUERY_NAME, "USCIS"));
+
+        String immigrationOfficeJson = callAPI(DAO.getRequestUrl(AppConstants.PLACES_QUERY_BASE, sb.toString()).toString());
+        Places immigrationOfficeContainer = convert(immigrationOfficeJson, Places.class);
+        List<Place> immigrationOfficeResults = immigrationOfficeContainer.getResults();
+        return immigrationOfficeResults;
+    }
+
+    private static List<Place> getDeptMotorVehiclesOffices(String location){
+
+        StringBuilder sb = getPlacesQueryBase(location);
+        sb.append(getQueryParam(AppConstants.PLACES_QUERY_NAME, "Department%20of%20Motor%20Vehicles"));
+
+        String deptMotorVehiclesJson = callAPI(DAO.getRequestUrl(AppConstants.PLACES_QUERY_BASE, sb.toString()).toString());
+        Places deptMotorVehiclesContainer = convert(deptMotorVehiclesJson, Places.class);
+        List<Place> deptMotorVehiclesResults = deptMotorVehiclesContainer.getResults();
+        return deptMotorVehiclesResults;
+    }
+
+    private static List<Place> getSocialSecurityOffices(String location){
+
+        StringBuilder socialSecurityOfficeQueryStem = getPlacesQueryBase(location);
+        socialSecurityOfficeQueryStem.append(getQueryParam(AppConstants.PLACES_QUERY_NAME, "Social%20Security"));
+
+        String socialSecurityOfficeJson = callAPI(DAO.getRequestUrl(AppConstants.PLACES_QUERY_BASE, socialSecurityOfficeQueryStem.toString()).toString());
+        Places socialSecurityContainer = convert(socialSecurityOfficeJson, Places.class);
+        List<Place> socialSecurityResults = socialSecurityContainer.getResults();
+        return socialSecurityResults;
+    }
+
+    private static StringBuilder getPlacesQueryBase(String location){
+
+        StringBuilder immigrationOfficeQueryStem = new StringBuilder();
+        immigrationOfficeQueryStem.append("key=" + AppConstantsPrivate.PLACES_API_KEY);
+        immigrationOfficeQueryStem.append(getQueryParam(AppConstants.PLACES_QUERY_RADIUS, "10000"));
+        immigrationOfficeQueryStem.append(getQueryParam(AppConstants.PLACES_QUERY_LOCATION, location));
+        immigrationOfficeQueryStem.append(getQueryParam("rankBy", "distance"));
+        return immigrationOfficeQueryStem;
+    }
+
+    /**
+     * Returns the city indices data from numbeo.
+     * @param location the city to use
+     * @return {@link CityIndices}
+     */
+    public static Places placesEndpoint(String location) {
+
+        Places essentials = new Places();
+        List<Place> essentialPlaces = new ArrayList<>();
+
+        List<Place> immigrationOfficePlaces = getImmigrationOffices(location);
+        List<Place> socialSecurityPlaces = getSocialSecurityOffices(location);
+        List<Place> deptMotoVehiclesPlaces = getDeptMotorVehiclesOffices(location);
+        List<Place> postOfficesPlaces = getPostOffices(location);
+
+        if (!immigrationOfficePlaces.isEmpty()){
+            // get closest immigration office
+            Place immigrationOffice = immigrationOfficePlaces.get(0);
+            immigrationOffice.setDescription("Get your permanent resident card, or citizenship");
+            essentialPlaces.add(immigrationOffice);
+        }
+
+        if (!socialSecurityPlaces.isEmpty()){
+            // get closest social security office
+            Place socialSecurityOffice = socialSecurityPlaces.get(0);
+            socialSecurityOffice.setDescription("Get your ability to work in the USA");
+            essentialPlaces.add(socialSecurityOffice);
+        }
+
+        if (!deptMotoVehiclesPlaces.isEmpty()){
+            // get closest social dept of motor vehicles
+            Place deptMotoVehicles = deptMotoVehiclesPlaces.get(0);
+            deptMotoVehicles.setDescription("Get your license or state id");
+            essentialPlaces.add(deptMotoVehicles);
+        }
+
+        if (!postOfficesPlaces.isEmpty()){
+            // get closest post office
+            Place postOffice = postOfficesPlaces.get(0);
+            postOffice.setDescription("Send and receive letters");
+            essentialPlaces.add(postOffice);
+        }
+
+        essentials.setResults(essentialPlaces);
+        return essentials;
     }
 
     /**
--- api/src/main/java/com/thewalkingdevs/api/model/Place.java
+++ api/src/main/java/com/thewalkingdevs/api/model/Place.java
@@ -3,13 +3,20 @@ package com.thewalkingdevs.api.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties({"icon", "id", "place_id", "reference", "scope", "types", "vicinity"})
+@JsonIgnoreProperties({"place_id", "reference", "scope", "types",
+        "vicinity", "rating", "opening_hours", "photos", "permanently_closed"})
 public class Place {
 
     @JsonProperty("geometry")
     private Geometry geometry = null;
     @JsonProperty("name")
     private String name = null;
+    @JsonProperty("id")
+    private String id = null;
+    @JsonProperty("icon")
+    private String icon = null;
+
+    private String description = null;
 
     public Geometry getGeometry() {
         return geometry;
@@ -25,5 +32,29 @@ public class Place {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
