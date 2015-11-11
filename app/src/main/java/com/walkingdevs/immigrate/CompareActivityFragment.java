5b674159b654767d31639b49f2cdb179f8a4e644
Levan Iordanishvili <levandev9053@gmail.com>
2015-11-11 05:59:51 -0500

    starter code for compare activity

---
+++ app/src/main/java/com/walkingdevs/immigrate/CompareActivityFragment.java
@@ -0,0 +1,22 @@
+package com.walkingdevs.immigrate;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class CompareActivityFragment extends Fragment {
+
+    public CompareActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compare, container, false);
+    }
+}
