5dcc9c5b2ff8716613ff5f0f57aff7c9539955eb
Philip Tucker <etechtour@gmail.com>
2015-11-11 05:10:00 -0500

    Maps do more!

--- app/src/main/java/com/walkingdevs/immigrate/NeighborhoodSliderFragment.java
+++ app/src/main/java/com/walkingdevs/immigrate/NeighborhoodSliderFragment.java
@@ -1,8 +1,9 @@
 package com.walkingdevs.immigrate;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.thewalkingdevs.api.myApi.model.Places;
+import com.thewalkingdevs.api.myApi.model.Geometry;
+import com.thewalkingdevs.api.myApi.model.Location;
 import com.thewalkingdevs.api.myApi.model.Place;
+import com.thewalkingdevs.api.myApi.model.Places;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,6 +63,8 @@ public class NeighborhoodSliderFragment extends Fragment {
 
     public class ViewHolder {
         public TextView placeNameText;
+        public TextView placeDescriptionText;
+        public TextView placeDistanceFromText;
     }
     public class PlaceAdapter extends ArrayAdapter<Place> {
 
@@ -70,17 +75,35 @@ public class NeighborhoodSliderFragment extends Fragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            Place place = getItem(position);
+            final Place place = getItem(position);
 
             // Check if an existing view is being reused, otherwise inflate the view
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_neighborhood, parent, false);
                 views = new ViewHolder();
                 views.placeNameText = (TextView)convertView.findViewById(R.id.item_place_name_text);
+                views.placeDescriptionText = (TextView) convertView.findViewById(R.id.item_place_description_text);
+                views.placeDistanceFromText = (TextView) convertView.findViewById(R.id.item_place_distance_text);
             }
             if(place != null) {
                 String name = place.getName();
                 views.placeNameText.setText(name);
+                String description = place.getDescription();
+                views.placeDescriptionText.setText(description);
+                String distance = String.format("%.2f", place.getDistFrom()) + " mi";
+                views.placeDistanceFromText.setText(distance);
+
+                convertView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Geometry geo = place.getGeometry();
+                        Location loc = geo.getLocation();
+
+                        Uri uri = Uri.parse("geo:0,0?q=" + loc.getLat()+"," + loc.getLng() + "(" + place.getName() + ")");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
             }
             // Return the completed view to render on screen
             return convertView;
